6a9e2121ae5a79722db3def951724670efdd39d3
unknown <jesumathu11@gmail.com>
2025-08-09 18:45:02 +0530

    first fiR1

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base:process.env.VITE_BASE_PATH || "/versal-react/settings/access"
+  base:process.env.VITE_BASE_PATH || "/versal-react/tree/main/frontend"
 })
